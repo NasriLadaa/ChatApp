1f84a70edc5cb8577a0c97c6a1dde1717fa54d51
Nasri_ladaa <nasrelada@gmail.com>
2025-02-26 23:50:49 +0200

    Create React App Project
    IndexComponent

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import IndexComponent from './components/IndexComponent'
+
+function App() {
+
+
+  return (
+    <>
+        <IndexComponent />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/IndexComponent.jsx
@@ -0,0 +1,13 @@
+
+
+const IndexComponent = ()=>{
+
+    return(
+        <>
+        <h1>Hello World!</h1>
+        </>
+    )
+
+}
+
+export default IndexComponent;
